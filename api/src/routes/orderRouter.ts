784bef555c482cdc01ffa88783f2a1438779d74d
samarkun <samarkun4@gmail.com>
2026-02-26 21:46:35 +0530

    orderRouter

--- api/src/routes/orderRouter.ts
+++ api/src/routes/orderRouter.ts
@@ -1,8 +1,22 @@
 import { Router } from "express";
+import { RedisClass } from "../redisQueue/Redis.js";
+import { CREATE_ORDER } from "../types/order.js";
 
 export const orderRouter = Router();
 
+orderRouter.post("/", async(req,res) => {
+    const {market , price , quantity , side , userId} = req.body;
 
-orderRouter.post("/", (req,res) => {
-    
+    const response = await RedisClass.getInstance().sendAndAwait({
+        type: CREATE_ORDER,
+        data: {
+            market,
+            price,
+            quantity,
+            side,
+            userId
+        }
+    }) as { payload: unknown };
+    res.json(response.payload);
 })
+
